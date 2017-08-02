7482128d166818facb7be48b43450ec52662d9a8
LingFeng.Chen.Cn <lingfeng.chen.cn@gmail.com>
2017-08-02 18:53:19 +0800

    Add trait
    add trait for for uuidForKey

---
+++ src/UuidForKey.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LingfengChenCN\Eloquent\Uuid\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait UuidForKey
+{
+    /**
+     * Stop  incrementing ID
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Hook 'creating'  method to set the key to be a UUID4 hex
+     *
+     * @return void
+     */
+    public static function bootUuidForKey()
+    {
+        static::creating(function ($model) {
+            $model->incrementing = false;
+            $model->attributes[$model->getKeyName()] = Uuid::uuid4()->getHex();
+        });
+    }
+}
